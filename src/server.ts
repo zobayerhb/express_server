437ffcee6819f7ffc5c3ebe7333f35a6db4fd8b4
zobayerhb <hosenzobayer55@gmail.com>
2025-12-12 19:32:11 +0600

    users create api create

--- src/server.ts
+++ src/server.ts
@@ -49,8 +49,27 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello next level developers");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  //   console.log(req.body);
+// Note: Users CRUD
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 
   res.status(201).json({
     success: true,
