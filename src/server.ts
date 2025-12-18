b9bc45322439be835e4664e54c2d882e54b5e463
zobayerhb <hosenzobayer55@gmail.com>
2025-12-18 20:56:37 +0600

    user todos create

--- src/server.ts
+++ src/server.ts
@@ -165,6 +165,54 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
 });
 
 //  TODO:  USER TODO CREATE
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Todos created successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// Note: get all todos
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+
+    res.status(200).json({
+      success: true,
+      message: "Todos retrive successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      succcess: false,
+      data: err.message,
+    });
+  }
+});
+
+// 404 not found
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
